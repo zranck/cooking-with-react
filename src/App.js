a1aa44c04a247c3e946b169e0eef207d1620bc0b
Zac Ranck <zac.ranck@gmail.com>
2022-07-19 20:40:24 -0600

    starting the counter project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 
 function App() {
-  return null
+  return (
+    <div>
+      {/* you cannot use class in JSX you must use className because class is reserved in js */}
+      <button>-</button>
+      <span>0</span>
+      <button>+</button>
+    </div>
+    
+  )
 }
 
 export default App;
