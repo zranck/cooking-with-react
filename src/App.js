d5e94118bad7979e59c769a8dffc83a371728a47
Zac Ranck <zac.ranck@gmail.com>
2022-07-23 07:52:14 -0600

    context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React, {useState} from 'react';
 import Counter from './Counter';
 import CounterHooks from './CounterHooks';
 
+export const ThemeContext = React.createContext();
+
+
 function App() {
+  const [theme, setTheme] = useState('red');
   return (
-    <>
+    <ThemeContext.Provider value={{backgroundColor: theme}}>
       Counter
       <Counter initialCount={0} />
       Counter Hooks
       <CounterHooks initialCount={1} />
-    </>
+      <button onClick={() => setTheme(prevTheme => {
+        return prevTheme === 'red' ? 'blue' : 'red';
+      })}>
+        Toggle Theme
+      </button>
+    </ ThemeContext.Provider>
   )
 }   
 
--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { ThemeContext } from './App';
 
 export default function CounterHooks({initialCount}) {
-    const [count, setcount] = useState(initialCount)
+    const [count, setcount] = useState(initialCount);
+    const style = useContext(ThemeContext)
     return (
         <div>
             {/* you cannot use class in JSX you must use className because class is reserved in js */}
-            <button onClick={() => setcount(prevCount => prevCount - 1)}>-</button>
+            <button style={style} onClick={() => setcount(prevCount => prevCount - 1)}>-</button>
             <span>{count}</span>
-            <button onClick={() => setcount(prevCount => prevCount + 1)}>+</button>
+            <button style={style} onClick={() => setcount(prevCount => prevCount + 1)}>+</button>
         </div>
     )
 }
