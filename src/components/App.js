dd7df372091eb8144102fb05e3142ff7405ca3f9
Zac Ranck <zac.ranck@gmail.com>
2022-07-24 20:15:23 -0600

    add and delete with context

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import RecipeList from './RecipeList';
 import '../css/app.css';
 import uuidv4 from 'uuid/v4'
 
+export const RecipeContext = React.createContext();
 
 function App() {
   const [recipes, setRecipes] = useState(sampleRecipes);
+
+  const recipeContextValue = {
+    handleRecipeAdd,
+    handleRecipeDelete
+  }
 
   function handleRecipeAdd() {
     const newRecipe = {
@@ -27,11 +33,10 @@ function App() {
   }
 
   return (
-    <RecipeList 
-    recipes={recipes}
-    handleRecipeAdd={handleRecipeAdd}
-    handleRecipeDelete={handleRecipeDelete}
-    />
+    <RecipeContext.Provider value={recipeContextValue}>
+      <RecipeList recipes={recipes} />
+    </RecipeContext.Provider>
+    
   )
 
   
